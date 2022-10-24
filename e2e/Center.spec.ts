08bfc04c622db22277cb26c39acea41cdb8618da
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-10-23 21:46:58 -0500

    feat: :sparkles: adds the <Center /> component

---
+++ e2e/Center.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('<Center />', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/center')
+  })
+
+  test('screenshot', async ({ page }) => {
+    expect(await page.screenshot()).toMatchSnapshot('center.png');
+  })
+})
--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
+import Center from './src/Center.astro'
 import Cluster from './src/Cluster.astro'
 import Component from './src/Component.astro'
 import Frame from './src/Frame.astro'
 import PadBox from './src/PadBox.astro'
 
 export {
+    Center,
     Cluster,
     Component,
     Frame,
